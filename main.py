f57d404629b54217f5e922b63b97f34818f1274c
luis <bisbicusluis@gmail.com>
2020-12-10 14:42:50 -0500

    Actualizacion

--- main.py
+++ main.py
@@ -8,6 +8,15 @@ import datetime
 from fastapi import FastAPI
 from fastapi import HTTPException
 api = FastAPI()
+from fastapi.middleware.cors import CORSMiddleware
+origins = [
+    "http://localhost.tiangolo.com", "https://localhost.tiangolo.com",
+    "http://localhost", "http://localhost:8080",
+]
+api.add_middleware(
+    CORSMiddleware, allow_origins=origins,
+    allow_credentials=True, allow_methods=["*"], allow_headers=["*"],
+)
 
 @api.post("/user/auth/")
 async def auth_user(user_in: UserIn):
